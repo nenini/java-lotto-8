bf85b11e758d09958ae845590a91da7c917c489f
nenini <kyr068600@gmail.com>
2025-11-01 19:23:22 +0900

    refactor(app): Application에서 LottoController로 실행 흐름 위임

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,24 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoController;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        OutputView.askPurchase();
-        InputView.readPurchase();
-
-        //TODO: 구매 로또 번호 출력
-
-        OutputView.askWinning();
-        InputView.readWinning();
-
-        OutputView.askBonus();
-        InputView.readBonus();
-
-        OutputView.printResultHeader();
-        //TODO: StatLine 출력
-
+        new LottoController().run();
     }
 }
